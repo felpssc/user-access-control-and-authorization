dc30df9900bfd92611e87d4ec595cdea789922fc
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-01-04 20:46:49 -0300

    ✨ Add User controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import {
+  listUsers,
+  create,
+  update,
+  deleteUserById,
+} from '../services/userService';
+
+const list = async (req: Request, res: Response) => {
+  try {
+    const users = await listUsers();
+    return res.status(200).json(users);
+  } catch (error: any) {
+    return res.json({ error: error.message });
+  }
+};
+
+const createUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await create({ name, email, password });
+    res.status(201).json(user);
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const { id } = req.user;
+
+  try {
+    const user = await update({ name }, id);
+    return res.status(200).json(user);
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    await deleteUserById(id);
+    return res.status(204).end();
+  } catch (error: any) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export {
+  list, createUser, updateUser, deleteUser,
+};
